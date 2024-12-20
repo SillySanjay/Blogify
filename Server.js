17709abe6fdee7e66d26f4d581474231cc3c7189
SillySanjay <sanjaysoni29032003@gmail.com>
2024-12-20 23:12:25 +0530

    first commit

--- Server.js
+++ Server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieparser = require('cookie-parser');
-
 const userroutes = require('./routes/user');
 const blogroutes = require('./routes/blog');
 const blog = require('./models/blog');
@@ -19,7 +18,7 @@ app.set('views',path.resolve('./views'));
 
 
 // DB connection
-mongoose.connect('mongodb://127.0.0.1:27017/Blogify')
+mongoose.connect('mongodb+srv://sillysanjay:7shanuu7@cluster0.8kun0.mongodb.net/Blogify')
 .then(()=> console.log('MongoDB Connection Established'))
 .catch((err)=>console.log(err));
 
