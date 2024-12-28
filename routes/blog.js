89c0fb87f6ada764cf2bd7ad24e6d7a85e053b4a
SillySanjay <sanjaysoni29032003@gmail.com>
2024-12-28 15:10:20 +0530

    updated

--- routes/blog.js
+++ routes/blog.js
@@ -8,7 +8,7 @@ const router = Router();
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         // Save files to the /tmp directory on Vercel
-        cb(null, '/public/tmp');
+        cb(null, '/tmp');
     },
     filename: function (req, file, cb) {
         const filename = `${Date.now()}-${file.originalname}`;
